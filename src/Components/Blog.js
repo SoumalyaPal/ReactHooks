8c3257573f40fe4b381d6c25fbe963009feb78cf
SOUMALYA PAL <soumalyapal721@gmail.com>
2025-06-19 23:31:53 +0530

    Update Blog.js

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,50 +1,48 @@
-import { useState, useRef, useEffect, useReducer} from "react";
-import { db } from "../firebaseInit";
-
-function blogsReducer(state, action){
-    switch(action.type){
-        case "ADD":
-            return [action.blog, ...state];
-        case "REMOVE":
-            return state.filter((blog,index) => index !== action.index);
-        default:
-            return [];
-    }
-}
+import { useState, useRef, useEffect } from "react";
+import {db} from "../firebaseInit";
+import { collection, addDoc, doc, setDoc} from "firebase/firestore"; 
 
 export default function Blog(){
 
     const [formData, setformData] = useState({title:"", content:""})
-    const [blogs, dispatch] =  useReducer(blogsReducer, []);
+    const [blogs, setBlogs] =  useState([]);
     const titleRef = useRef(null);
 
     useEffect(() => {
-        titleRef.current.focus();
+        titleRef.current.focus()
     },[]);
 
-    useEffect(() => {
-        if(blogs.length && blogs[0].title){
-            document.title = blogs[0].title;
-        } else {
-            document.title = "No Blogs!!!"
-        }
-    },[blogs]);
-
-    function handleSubmit(e){
+    async function handleSubmit(e){
         e.preventDefault();
-
-        // setBlogs([{title: formData.title,content:formData.content}, ...blogs]);
-        dispatch({type: "ADD", blog:{title: formData.title,content:formData.content}});
-        setformData({title:"", content:""});
         titleRef.current.focus();
 
-        console.log(blogs);
+        setBlogs([{title: formData.title,content:formData.content}, ...blogs]);
+
+        //addDoc either this way
+
+        //await addDoc(collection(db, "blogs"), {
+            // title: formData.title,
+            // content: formData.content,
+            // createdOn: new Date()
+        // })
+
+        //addDoc either this way
+        const docRef = doc(collection(db, "blogs"));
+            
+        await setDoc(docRef, {
+                title: formData.title,
+                content: formData.content,
+                createdOn: new Date()
+            });
+        setformData({title: "", content: ""});
     }
-    function removeBlog(i){
-        // setBlogs( blogs.filter((blog,index)=> index !== i));
-        dispatch({type:"REMOVE", index:i});
+
+    async function removeBlog(i){
+
+        setBlogs( blogs.filter((blog,index)=> index !== i));
  
      }
+
     return(
         <>
         <h1>Write a Blog!</h1>
@@ -53,32 +51,33 @@ export default function Blog(){
                 <Row label="Title">
                         <input className="input"
                                 placeholder="Enter the Title of the Blog here.."
+                                ref = {titleRef}
                                 value={formData.title}
-                                ref={titleRef}
                                 onChange = {(e) => setformData({title: e.target.value, content:formData.content})}
                         />
                 </Row >
                 <Row label="Content">
                         <textarea className="input content"
                                 placeholder="Content of the Blog goes here.."
-                                value={formData.content}
                                 required
+                                value={formData.content}
                                 onChange = {(e) => setformData({title: formData.title,content: e.target.value})}
                         />
                 </Row >
                 <button className = "btn">ADD</button>
-            </form>  
+            </form>
+                     
         </div>
         <hr/>
         <h2> Blogs </h2>
         {blogs.map((blog,i) => (
-            <div className="blog">
+            <div className="blog" key={i}>
                 <h3>{blog.title}</h3>
                 <hr/>
                 <p>{blog.content}</p>
                 <div className="blog-btn">
                         <button onClick={() => {
-                            removeBlog(i)
+                             removeBlog(i)
                         }}
                         className="btn remove">
                             Delete
